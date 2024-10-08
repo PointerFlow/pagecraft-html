6f9ab78ed28739f04127f8773fdd44463893b602
santo mitro <santo.techlyn@gmail.com>
2024-10-08 17:36:21 +0600

    page 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,7 +87,7 @@ var icon_with_text_slider = new Swiper(".icon_with_text_slider", {
     },
 });
 
-
+//brand-slider
 var swiper = new Swiper(".brand-slider", {
     slidesPerView: 2,
     spaceBetween: 30,
@@ -121,4 +121,127 @@ var swiper = new Swiper(".product-slider", {
     },
     slidesPerView: 4,
     spaceBetween: 30,
+});
+
+
+//client testimonial
+var swiper = new Swiper(".pf-client-testimonial", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: ".client-swiper-button-next",
+        prevEl: ".client-swiper-button-prev",
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+});
+
+
+//pf-brand-2-slider
+var swiper = new Swiper(".pf-brand-2-slider", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    speed: 1000,
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        992: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+        },
+    },
+});
+
+//pf-icon-with-text-slider
+var icon_with_text_slider = new Swiper(".icon_with_text_slider-2", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
+});
+
+
+//image-blog-slider
+var swiper = new Swiper(".image-blog-slider", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: ".client-swiper-button-next",
+        prevEl: ".client-swiper-button-prev",
+    },
+    breakpoints: {
+
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        1200: {
+            slidesPerView: 5,
+            spaceBetween: 10,
+        },
+    },
+});
+
+//image-blog-slider
+var swiper = new Swiper(".category-shop-slider", {
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".category-shop-swiper-button-next",
+        prevEl: ".category-shop-swiper-button-prev",
+    },
+    spaceBetween: 0,
+    breakpoints: {
+
+        576: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+    },
 });
